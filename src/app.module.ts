e494afa9b602449a77556f22e6f4505886112647
Felipe Pessina <fs.pessina@gmail.com>
2023-06-18 21:25:06 +0600

    Firebase auth working

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
+import { FirebaseAuthStrategy } from './auth/strategy';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, FirebaseAuthStrategy],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma } from '@prisma/client';
+import { FirebaseAuthGuard } from 'src/auth/guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(FirebaseAuthGuard)
   @Post()
   async createUser(@Body() userData: Prisma.UserCreateInput) {
     console.log('userData', userData);
