6e21bd86ac49262977946057d919ef79bf11ba4a
Felipe Pessina <fs.pessina@gmail.com>
2023-07-10 14:53:50 +0600

    Add all modules back

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { AuthStrategy } from './auth/strategy';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { VisitModule } from './visit/visit.module';
 
 @Module({
-  imports: [],
+  imports: [DoctorModule, PatientModule, VisitModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, AuthStrategy],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Felipe test here';
+    return 'Hello Felipe, test code here!';
   }
 }
