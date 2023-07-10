f0e8d70afede03a57cbc9b15efd462405838567e
Felipe Pessina <fs.pessina@gmail.com>
2023-07-10 14:49:07 +0600

    Simplify app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
-import { AuthStrategy } from './auth/strategy';
-import { DoctorModule } from './doctor/doctor.module';
-import { PatientModule } from './patient/patient.module';
-import { VisitModule } from './visit/visit.module';
 
 @Module({
-  imports: [DoctorModule, PatientModule, VisitModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService, AuthStrategy],
+  providers: [AppService],
 })
 export class AppModule {}
