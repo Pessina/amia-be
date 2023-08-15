4d4ca0885fc303d5ed6a78114f024ace792e6d5d
Felipe Pessina <fs.pessina@gmail.com>
2023-08-15 23:53:15 +0600

    Add event to process long running api call

--- src/visit/visit.controller.ts
+++ src/visit/visit.controller.ts
@@ -5,6 +5,7 @@ import {
   Param,
   Post,
   Req,
+  Res,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -13,10 +14,10 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { VisitService } from './visit.service';
 import { AppAuthGuard } from 'src/auth/guard';
 import { AuthRequest } from 'src/types/AuthRequest';
-import { ProcessVisitRecordingResponse } from './visit.types';
 import { PatientVisitSummary } from 'src/services/llm/prompts/patientVisit.promp';
 import { Visit } from '@prisma/client';
 import { parseISO } from 'date-fns';
+import { Response } from 'express';
 
 @Controller('visit')
 export class VisitController {
@@ -45,15 +46,76 @@ export class VisitController {
     @UploadedFile() audio: Express.Multer.File,
     @Body('patientId') patientId: string,
     @Body('timestamp') timestamp: string,
-    @Body('timezone') timezone: string
-  ): Promise<ProcessVisitRecordingResponse> {
-    return await this.visit.processVisitRecording(
-      req.user.email,
-      audio,
-      parseInt(patientId),
-      timestamp,
-      timezone
-    );
+    @Body('timezone') timezone: string,
+    @Res() res: Response
+  ): Promise<void> {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    try {
+      // const response = await this.visit.processVisitRecording(
+      //   req.user.email,
+      //   audio,
+      //   parseInt(patientId),
+      //   timestamp,
+      //   timezone
+      // );
+      const response = {
+        medicalRecord: {
+          topics: [
+            {
+              title: 'Visit Summary',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary1',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary3',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary4',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary5',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary6',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary7',
+              content: "Here's a summary of your visit",
+            },
+            {
+              title: 'Visit Summary8',
+              content: "Here's a summary of your visit",
+            },
+          ],
+        },
+      };
+
+      res.write('event: success\n');
+      res.write(`data: ${JSON.stringify(response)}\n\n`);
+    } catch (error) {
+      res.write('event: error\n');
+      res.write(
+        `data: ${JSON.stringify({
+          errorCode: 'PROCESSING_ERROR',
+          errorMessage: error.message,
+          errorDetails: {
+            reason: 'Failed to process visit recording',
+          },
+        })}\n\n`
+      );
+    } finally {
+      res.end();
+    }
   }
 
   // TODO: remove this endpoint, it's only for testing
